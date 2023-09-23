963f374b1a91ca2a639d9436e0898b8def1e49e7
Arnaldo Muñoz <kings31lendy@gmail.com>
2023-09-23 19:54:14 -0400

    Estructura Básica del proyecto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,37 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home-page/home/home.component';
+import { ActivitiesComponent } from './pages/services-page/activities/activities.component';
+import { AboutComponent } from './pages/about-page/about/about.component';
+import { ContactComponent } from './pages/contact-page/contact/contact.component';
+import { BlogComponent } from './pages/blog-page/blog/blog.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'home',
+    component: HomeComponent
+  },
+  {
+    path:'activities',
+    component: ActivitiesComponent
+  },
+  {
+    path:'about',
+    component: AboutComponent
+  },
+  {
+    path:'contact',
+    component: ContactComponent
+  },
+  {
+    path:'blog',
+    component: BlogComponent
+  },
+  {
+    path:'**',
+    redirectTo: 'home'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/shared/navbar/navbar.component.ts
@@ -0,0 +1,19 @@
+import { Component, ElementRef, ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent {
+  @ViewChild("menu") menu!:ElementRef;
+
+  showwMenu(){
+    if(this.menu.nativeElement.classList.contains("navigation-container-active")){
+      this.menu.nativeElement.classList.remove("navigation-container-active");
+    }else{
+      this.menu.nativeElement.classList.add("navigation-container-active")
+    }
+
+  }
+}
